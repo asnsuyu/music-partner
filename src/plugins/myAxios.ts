0dd733e875ac5049a2affb3937584f905c36f78b
asnpro <920569392@qq.com>
2024-09-13 12:22:21 +0800

    fix: 修改线上请求地址

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -1,30 +1,30 @@
-import axios, {AxiosInstance} from "axios";
+import axios, { AxiosInstance } from "axios"
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development"
 
 const myAxios: AxiosInstance = axios.create({
-    baseURL: isDev ? 'http://localhost:8080/api' : 'http://fordemo.asia/api',
-});
+	baseURL: isDev ? "http://localhost:8080/api" : "https://user-center-backend-120815-6-1329229128.sh.run.tcloudbase.com/api",
+})
 
-myAxios.defaults.withCredentials = true; // 配置为true
+myAxios.defaults.withCredentials = true // 配置为true
 
 // 请求拦截器
-myAxios.interceptors.request.use(function (config) {
-    return config;
-}, function (error) {
-    return Promise.reject(error);
-});
+myAxios.interceptors.request.use(function(config) {
+	return config
+}, function(error) {
+	return Promise.reject(error)
+})
 
 // 响应拦截器
-myAxios.interceptors.response.use(function (response) {
-    // 未登录则跳转到登录页
-    if (response?.data?.code === 40100) {
-        const redirectUrl = window.location.href;
-        window.location.href = `/#/user/login?redirect=${redirectUrl}`;
-    }
-    return response.data;
-}, function (error) {
-    return Promise.reject(error);
-});
+myAxios.interceptors.response.use(function(response) {
+	// 未登录则跳转到登录页
+	if (response?.data?.code === 40100) {
+		const redirectUrl = window.location.href
+		window.location.href = `/#/user/login?redirect=${redirectUrl}`
+	}
+	return response.data
+}, function(error) {
+	return Promise.reject(error)
+})
 
-export default myAxios;
+export default myAxios
